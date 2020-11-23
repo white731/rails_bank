d5c831a636b5912aa4ae990797b109b87a7c5283
Landon Whitesides <whitesideslandon@gmail.com>
2020-11-23 11:55:33 -0700

    create account working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(first_name: "Harold", last_name: 'James', email:"harold@gmail.com", password: "123456")
+
+user1.accounts.create(name:'Checking', balance: 29.95)
+user1.accounts.create(name:'Savings', balance: 2229.95)
+user1.accounts.create(name:'Vacation', balance: 9124567.78)
